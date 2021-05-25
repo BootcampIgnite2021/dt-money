967828d29211ac7d073e34c336eb4aacc35417fc
Francisco Junior <franciscojn49@gmail.com>
2021-05-24 21:53:20 -0300

    feat: configured miragejs and axios.

---
+++ src/data/dataTransactions.ts
@@ -0,0 +1,34 @@
+export const dataTransactions = [
+  {
+    id: 1,
+    title: "Transaction 1",
+    amount: 400,
+    type: "deposit",
+    category: "food",
+    createdAt: new Date(),
+  },
+  {
+    id: 2,
+    title: "Transaction 2",
+    amount: 400,
+    type: "deposit",
+    category: "food",
+    createdAt: new Date(),
+  },
+  {
+    id: 3,
+    title: "Transaction 3",
+    amount: 400,
+    type: "deposit",
+    category: "food",
+    createdAt: new Date(),
+  },
+  {
+    id: 4,
+    title: "Transaction 4",
+    amount: 400,
+    type: "deposit",
+    category: "food",
+    createdAt: new Date(),
+  },
+];
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import App from './App';
+
+import { dataTransactions } from './data/dataTransactions';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return dataTransactions;
+    })
+  }
+
+})
 
 ReactDOM.render(
   <React.StrictMode>
